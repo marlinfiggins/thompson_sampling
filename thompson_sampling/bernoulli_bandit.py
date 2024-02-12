92635ddb989fa779696a36283add33a041dfd6ae
marlinfiggins <marlinfiggins@gmail.com>
2024-02-12 15:30:43 -0800

    Updating bernoulli bandit for clarity

--- thompson_sampling/bernoulli_bandit.py
+++ thompson_sampling/bernoulli_bandit.py
@@ -8,34 +8,109 @@ class BernoulliBandit:
         self.probs = probs
         self.reward_dist = [Bernoulli(p) for p in self.probs]
 
-    def sample(self, arm):
+    def pull_arm(self, arm):
         return self.reward_dist[arm].rvs()
 
 
+class EpsilonGreedy:
+    """
+    Epsilon-greedy agent for solving multi-armed bandit problems.
+
+    Parameters:
+    - n_arms (int): The number of arms in the bandit.
+    - epsilon (float): Probability of selecting a random arm for exploration.
+    """
+
+    def __init__(self, n_arms: int, eps: float):
+        self.n_arms = n_arms
+        self.eps = eps
+        self.expected_reward = np.zeros(n_arms)
+        self.times_chosen = np.zeros(n_arms)
+
+    def update_q(self, arm: int, reward: float):
+        self.times_chosen[arm] += 1
+
+        # Online update for the mean reward
+        self.expected_reward[arm] += (
+            reward - self.expected_reward[arm]
+        ) / self.times_chosen[arm]
+        return None
+
+    def choose_arm(self):
+        # Explore with probability epsilon
+        if np.random.uniform() < self.eps:
+            return np.random.choice(self.n_arms)
+        else:
+            return np.argmax(self.expected_reward)
+
+
+class UCB:
+    """
+    UCB (Upper Confidence Bound) agent for solving multi-armed bandit problems.
+
+    Parameters:
+    - c (float): Controls the balance between exploration and exploitation.
+    - n_arms (int): The number of arms in the bandit.
+    """
+
+    def __init__(self, n_arms: int, c: float):
+        self.n_arms = n_arms
+        self.c = c
+        self.expected_reward = np.zeros(n_arms)
+        self.times_chosen = np.zeros(n_arms)
+        self.total_actions = 0
+
+    def update_q_value(self, arm, reward):
+        # Update Q-value for the pulled arm
+        self.times_chosen[arm] += 1
+        # Online update for the expected reward
+        self.expected_reward[arm] += (
+            reward - self.expected_reward[arm]
+        ) / self.times_chosen[arm]
+
+    def choose_arm(self):
+        ucb_values = self.expected_reward + self.c * np.sqrt(
+            np.log(self.total_actions + 1) / (self.times_chosen + 1e-4)
+        )
+        return np.argmax(ucb_values)
+
+
 class ThompsonSampling:
+    """
+    Thompson Sampling agent for solving multi-armed bandit problems.
+
+    Parameters:
+    - n_arms (int): The number of arms in the bandit.
+
+    Attributes:
+    - n_arms (int): The number of arms in the bandit.
+    - alpha (float): Alpha parameters of the beta distribution for each arm.
+    - beta (float): Beta parameters of the beta distribution for each arm.
+    """
     def __init__(
         self,
-        n_arms,
+        n_arms: int,
         alpha=1.0,
         beta=1.0,
     ):
         self.n_arms = n_arms
-        self.alpha, self.beta = alpha, beta  # Prior alpha and beta
+        self.alpha = np.ones(n_arms) * alpha
+        self.beta = np.ones(n_arms) * beta  # Prior alpha and beta
 
-    def get_theta(self, successes, failures):
-        return [
-            Beta(self.alpha + successes[k], self.beta + failures[k])
-            for k in range(self.n_arms)
-        ]
+    @property
+    def _theta(self):
+        return [Beta(self.alpha, self.beta) for k in range(self.n_arms)]
 
-    def sample(self, successes, failures):
-        # Update theta given successes and failures
-        self.theta = self.get_theta(successes, failures)
+    def update_q(self, arm, reward):
+        if reward == 0:
+            self.alpha[arm] += 1
+        else:
+            self.beta[arm] += 1
+        return None
 
+    def choose_arm(self):
         # Draw posterior prob of success
-        theta_sample = np.array([theta_rv.rvs() for theta_rv in self.theta])
+        theta_sample = np.array([theta_rv.rvs() for theta_rv in self._theta])
 
         # Select highest theta arm
-        selected_arm = np.argmax(theta_sample)
-
-        return selected_arm
+        return np.argmax(theta_sample)
